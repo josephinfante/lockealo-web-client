7fcc5264bd0f96ff4576252b7181156c448ea36c
Joseph Infante <josephinfante.y@gmail.com>
2025-01-17 12:56:53 -0500

    fixing display issue

--- app/(auth)/otp/page.tsx
+++ app/(auth)/otp/page.tsx
@@ -2,13 +2,15 @@ import OTPForm from './otp-form'
 
 function Page() {
 	return (
-		<div className="max-w-md w-full mx-auto flex flex-col gap-4">
-			<div>
-				<h1 className="text-2xl font-medium">Verify your account.</h1>
-				<p className="text-sm text-muted-foreground">We sent a one-time code to your email.</p>
+		<main className="flex-1 flex items-center justify-center max-w-7xl py-4 px-4 sm:px-6 lg:px-8 mx-auto w-full">
+			<div className="max-w-md w-full mx-auto flex flex-col gap-4">
+				<div>
+					<h1 className="text-2xl font-medium">Verify your account.</h1>
+					<p className="text-sm text-muted-foreground">We sent a one-time code to your email.</p>
+				</div>
+				<OTPForm />
 			</div>
-			<OTPForm />
-		</div>
+		</main>
 	)
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,19 +3,21 @@ import SignUpForm from './sign-up-form'
 
 function Page() {
 	return (
-		<div className="max-w-md w-full mx-auto flex flex-col gap-4">
-			<div>
-				<h1 className="text-2xl font-medium">Sign up.</h1>
-				<p className="text-sm text-muted-foreground">Fill in your information.</p>
+		<main className="flex-1 flex items-center justify-center max-w-7xl py-4 px-4 sm:px-6 lg:px-8 mx-auto w-full">
+			<div className="max-w-md w-full mx-auto flex flex-col gap-4">
+				<div>
+					<h1 className="text-2xl font-medium">Sign up.</h1>
+					<p className="text-sm text-muted-foreground">Fill in your information.</p>
+				</div>
+				<SignUpForm />
+				<p className="text-sm text-center">
+					You already have an account?
+					<Link href={'/sign-in'} className="ml-1 text-primary underline font-medium">
+						Sign in
+					</Link>
+				</p>
 			</div>
-			<SignUpForm />
-			<p className="text-sm text-center">
-				You already have an account?
-				<Link href={'/sign-in'} className="ml-1 text-primary underline font-medium">
-					Sign in
-				</Link>
-			</p>
-		</div>
+		</main>
 	)
 }
 
